c164b94730251e4d011bb9fc1908d7f520eec33e
Matt Kane <matt@gatsbyjs.com>
2021-06-23 12:08:21 +0100

    chore: use gh branch

--- apps/tuskdesk/next.config.js
+++ apps/tuskdesk/next.config.js
@@ -3,6 +3,7 @@ const withNx = require('@nrwl/next/plugins/with-nx');
 
 module.exports = withNx({
   distDir: '.dist',
+  target: 'serverless',
   nx: {
     // Set this to false if you do not want to use SVGR
     // See: https://github.com/gregberge/svgr
